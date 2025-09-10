f925ee41afe78ec975aac448deb6c5cbef58b14d
sancoredu <s.cortesvaillo@edu.gva.es>
2025-09-10 19:03:13 +0200

    Add timestamp to endpont hello

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import datetime
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/hello")
 async def hello_world():
-    return {"message": "Hello, world!"}
+    return {
+        "message": "Hello, world!",
+        "timestamp": str(datetime.datetime.now(datetime.timezone.utc)),
+        }
