8ecb859fd913a268c901654851329875cc5f6e74
sancoredu <s.cortesvaillo@edu.gva.es>
2025-09-11 09:50:21 +0200

    Add API version to endpoint hello

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import datetime
 
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="Example API",
+    version="0.2.0",
+)
 
 @app.get("/hello")
 async def hello_world():
     return {
         "message": "Hello, world!",
         "timestamp": str(datetime.datetime.now(datetime.timezone.utc)),
+        "version": app.version,
         }
